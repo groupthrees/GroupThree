77e2525c1c2982d03f1ce8b37aefbbd11d8373b5
GitHubCorderLai <51562165+GitHubCorderLai@users.noreply.github.com>
2019-06-11 20:53:33 +0800

    Wine

---
+++ Brewmaster.Alcohol.Api/Brewmaster.Alcohol.Api/Startup.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Brewmaster.Alcohol.IRepository;
+using Brewmaster.Alcohol.Repository;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Brewmaster.Alcohol.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // 注册接口和实现类的映射关系  
+            services.AddScoped<ITestRepository, TestRepository>();
+
+            //自动生成 设置MVC 兼容性版本
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            //注册跨域服务，允许所有来源
+            services.AddCors(options =>
+                options.AddPolicy("AllowAnyCors",
+                p => p.AllowAnyOrigin())
+                );
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //允许跨域访问
+            app.UseCors("AllowAnyCors");
+            app.UseMvc();
+        }
+    }
+}
