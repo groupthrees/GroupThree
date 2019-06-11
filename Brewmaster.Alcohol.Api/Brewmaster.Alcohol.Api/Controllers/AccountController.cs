188b7ebe3d294aa127a8af2ec4e2000a79eaedf7
GitHubCorderLai <51562165+GitHubCorderLai@users.noreply.github.com>
2019-06-11 16:08:33 +0800

    项目

---
+++ Brewmaster.Alcohol.Api/Brewmaster.Alcohol.Api/Controllers/AccountController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Brewmaster.Alcohol.Api.Controllers
+{
+    public class AccountController : BaseController
+    {
+        /// <summary>
+        /// 页面初始化
+        /// </summary>
+        /// <param name="returnUrl"></param>
+        /// <returns></returns>
+        public IActionResult Login(string returnUrl = null)
+        {
+            TempData["returnUrl"] = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult LoginDo(UserInfo user, string returnUrl = null)
+        {
+            //验证用户是否登录
+            const string errorMessage = "用户名或密码错误！";
+            if (user == null)
+            {
+                return BadRequest(errorMessage);
+            }
+            var tmpUser = new UserInfo().GetUserList().FirstOrDefault(m => m.UserName == user.UserName && m.Password == user.Password);
+            if (tmpUser?.Password != user.Password)
+            {
+                return BadRequest(errorMessage);
+            }
+
+            //写入缓存
+            WriteCookie(tmpUser);
+
+            //判断是否返回前页
+            if (returnUrl == null)
+            {
+                returnUrl = TempData["returnUrl"]?.ToString();
+            }
+            if (returnUrl != null)
+            {
+                return Redirect(returnUrl);
+            }
+
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+
+        /// <summary>
+        /// 退出
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction(nameof(AccountController.Login), "Account");
+        }
+    }
+}
---
+++ Brewmaster.Alcohol.Api/Brewmaster.Alcohol.Repository/DapperHelper.cs
@@ -0,0 +1,59 @@
+﻿using Brewmaster.Alcohol.Model;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+using Dapper;
+using System;
+using System.Linq;
+
+namespace Brewmaster.Alcohol.Repository
+{
+    public class DapperHelper
+    {
+        private const string connString = "server=.;database=Core;uid=sa;pwd=1";
+
+        ///// <summary>
+        ///// 获取连接数据库的字符串
+        ///// </summary>
+        ///// <returns></returns>
+        //public static System.Data.SqlClient.SqlConnection GetConnString()
+        //{
+        //    return new System.Data.SqlClient.SqlConnection(connString);
+        //}
+
+        ///// <summary>
+        ///// 公共分页方法
+        ///// </summary>
+        ///// <typeparam name="T">表名</typeparam>
+        ///// <param name="criteria">分页对象</param>
+        ///// <returns></returns>
+        //public static PageDataView<T> GetPageData<T>(PageCriteria criteria)
+        //{
+        //    using (SqlConnection conn = GetConnString())
+        //    {
+        //        conn.Open();
+
+        //        DynamicParameters parms = new DynamicParameters();
+        //        parms.Add("@TableName", criteria.TableName, DbType.String, ParameterDirection.Input, null);
+        //        parms.Add("@PrimaryKey", criteria.PrimaryKey, DbType.String, ParameterDirection.Input, null);
+        //        parms.Add("@Fields", criteria.Fields, DbType.String, ParameterDirection.Input, null);
+        //        parms.Add("@Condition", criteria.Condition, DbType.String, ParameterDirection.Input, null);
+        //        parms.Add("@CurrentPage", criteria.CurrentPage, DbType.Int32, ParameterDirection.Input, null);
+        //        parms.Add("@PageSize", criteria.PageSize, DbType.Int32, ParameterDirection.Input, null);
+        //        parms.Add("@Sort", criteria.Sort, DbType.String, ParameterDirection.Input, null);
+        //        parms.Add("@RecordCount", criteria.RecordCount, DbType.Int32, ParameterDirection.Output, null);
+
+        //        string proName = "ProcGetPageData";
+        //        var pageData = new PageDataView<T>();
+        //        pageData.Items = conn.Query<T>(proName, parms, null, false, null, CommandType.StoredProcedure).ToList();
+        //        conn.Close();
+
+        //        pageData.TotalNum = parms.Get<int>("RecordCount");
+        //        pageData.TotalPageCount = Convert.ToInt32(Math.Ceiling(pageData.TotalNum * 1.0 / criteria.PageSize));
+        //        pageData.CurrentPage = criteria.CurrentPage > pageData.TotalPageCount ? pageData.TotalPageCount : criteria.CurrentPage;
+        //        return pageData;
+        //    }
+        //}
+    }
+}
