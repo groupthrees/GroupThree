8e94b483dd551af4226b79b447feb928b02e63ca
senyu0520 <51561807+senyu0520@users.noreply.github.com>
2019-06-12 21:23:22 +0800

    SS

---
+++ Brewmaster.Alcohol.Api/Brewmaster.Alcohol.Api/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using Brewmaster.Alcohol.IRepository;
+using Brewmaster.Alcohol.Repository;
+using Brewmaster.Alcohol.Model;
+namespace Brewmaster.Alcohol.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        /// <summary>
+        /// 定义私有变量
+        /// </summary>
+        private readonly IUserRepository _UserRepository;
+
+        /// <summary>
+        /// 构造函数注入
+        /// </summary>
+        /// <param name="studentRepository"></param>
+        public UserController(IUserRepository UserRepository)
+        {
+            _UserRepository = UserRepository;
+        }
+
+        /// <summary>
+        /// 注册
+        /// </summary>
+        /// <param name="users"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public int Resigt(Users users)
+        {
+            int result = _UserRepository.Resigt(users);
+            return result;
+        }
+    }
+}
