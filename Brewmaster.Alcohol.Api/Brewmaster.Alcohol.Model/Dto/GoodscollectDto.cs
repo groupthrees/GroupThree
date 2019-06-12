7fc3091890eb93d25b60e6c5f456d0b8cd9d28dd
GitHubCorderLai <51562165+GitHubCorderLai@users.noreply.github.com>
2019-06-12 21:08:01 +0800

    dto

---
+++ Brewmaster.Alcohol.Api/Brewmaster.Alcohol.Model/Dto/GoodscollectDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Brewmaster.Alcohol.Model
+{
+    //商品详情页 Dto
+    public class GoodscollectDto
+    {
+        //是否收藏
+        public int IsCollect { get; set; }
+        //下方图片放大
+        public imgs img { get; set; }
+    }
+  public  class imgs
+    {
+
+        public List<string> img { get; set; }
+    }
+}
