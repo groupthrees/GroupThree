d7cbc7011beae1abeb67105c7c83eeeecf9ee2e1
huakunkun <HKK20000115@162.com>
2019-06-12 10:13:46 +0800

    华坤坤  我的收藏

---
+++ Brewmaster.Alcohol.Client/Brewmaster.Alcohol.Client/Controllers/个人中心/MyCollectController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Brewmaster.Alcohol.Client.Controllers.个人中心
+{
+    //我的收藏
+    public class MyCollectController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
