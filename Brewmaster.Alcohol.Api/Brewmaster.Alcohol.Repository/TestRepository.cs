d60208b4529aaf7170ad6e1000a692f8d7997b67
GitHubCorderLai <51562165+GitHubCorderLai@users.noreply.github.com>
2019-06-12 19:01:23 +0800

    data.sqlclient 赖少乾 6-12 19:00

--- Brewmaster.Alcohol.Api/Brewmaster.Alcohol.Repository/TestRepository.cs
+++ Brewmaster.Alcohol.Api/Brewmaster.Alcohol.Repository/TestRepository.cs
@@ -2,13 +2,17 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-
+using System.Data.SqlClient;
 namespace Brewmaster.Alcohol.Repository
 {
     public class TestRepository : ITestRepository
     {
         public int test(int id)
         {
+            using (SqlConnection s = new SqlConnection())
+            {
+
+            }
             return id;
         }
     }
