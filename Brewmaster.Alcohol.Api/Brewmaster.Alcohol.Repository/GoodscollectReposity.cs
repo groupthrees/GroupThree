9304d952a10effa6b4bd28ee1030267930cefff1
GitHubCorderLai <51562165+GitHubCorderLai@users.noreply.github.com>
2019-06-13 14:03:48 +0800

    Update GoodscollectReposity.cs

--- Brewmaster.Alcohol.Api/Brewmaster.Alcohol.Repository/GoodscollectReposity.cs
+++ Brewmaster.Alcohol.Api/Brewmaster.Alcohol.Repository/GoodscollectReposity.cs
@@ -5,7 +5,7 @@ using Brewmaster.Alcohol.IRepository;
 using Brewmaster.Alcohol.Model;
 using Dapper;
 using System.Data.SqlClient;
-//using 
+using MySql.Data.MySqlClient;
 namespace Brewmaster.Alcohol.Repository
 {
     public class GoodscollectReposity : IGoodsCollectReposity
@@ -20,19 +20,15 @@ namespace Brewmaster.Alcohol.Repository
         /// <returns></returns>
         public  GoodscollectDto GetGoodscollectDto(int id, int goodsId)
         {
-
-
-
-
-
             //是否收藏
-            string sql1 = $"select * from collect where Goods={goodsId} and UserId={id}";
+            string sql1 = $"select * from collect where GoodsId={goodsId} and UserId={id}";
             //根据商品Id查询该商品的图片
-            string sql2 = $"select * from collect where Goods={goodsId} and UserId={id}";
-            //using (MySqlConnection )
-            //{
+            string sql2 = $"select pictureUrl from prcture where GoodsId={goodsId} ";
+            string sql3 = $"select * from ";
+            using (MySqlConnection con=new MySqlConnection(connStr))
+            {
 
-            //}
+            }
             return null;
         }
 
