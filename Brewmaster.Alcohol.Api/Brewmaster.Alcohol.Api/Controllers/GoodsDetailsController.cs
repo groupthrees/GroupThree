0cdcd8b6c37c7755ae4833dc9e4084913ca2a9f2
GitHubCorderLai <51562165+GitHubCorderLai@users.noreply.github.com>
2019-06-13 09:45:27 +0800

    Create GoodsDetailsController.cs

---
+++ Brewmaster.Alcohol.Api/Brewmaster.Alcohol.Api/Controllers/GoodsDetailsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Brewmaster.Alcohol.Model;
+//商品详情页控制器
+namespace Brewmaster.Alcohol.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GoodsDetailsController : ControllerBase
+    {
+
+    }
+}
