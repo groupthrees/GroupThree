a9dd186f4ccd39eb8cdc8d311f4de69790d1cb2a
qq3490667734 <51558362+qq3490667734@users.noreply.github.com>
2019-06-14 11:48:21 +0800

    范涛涛

--- Brewmaster.Alcohol.Client/Brewmaster.Alcohol.Client/Controllers/搜索/GoodsSearch.cs
+++ Brewmaster.Alcohol.Client/Brewmaster.Alcohol.Client/Controllers/搜索/GoodsSearch.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Brewmaster.Alcohol.Catch;
+using Brewmaster.Alcohol.Client.Models;
 using Microsoft.AspNetCore.Mvc;
-
+using Newtonsoft.Json;
 
 
 namespace Brewmaster.Alcohol.Client.Controllers
@@ -15,7 +17,25 @@ namespace Brewmaster.Alcohol.Client.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
+            ViewBag.data=GetBrand();
+            ViewBag.list=GetGoodsType();
             return View();
+        }
+
+        public List<Brand> GetBrand()
+        {
+            ApiHelper apiHelper=new ApiHelper();
+            var str= apiHelper.GetApiResult("get", "Brand/GetBrandName",null);
+            List<Brand> list = JsonConvert.DeserializeObject<List<Brand>>(str);
+            return list;
+        }
+
+        public List<GoodsType> GetGoodsType()
+        {
+            ApiHelper apiHelper = new ApiHelper();
+            var str = apiHelper.GetApiResult("get", "GoodsType/GetGoodsTypeName", null);
+             List<GoodsType> list = JsonConvert.DeserializeObject<List<GoodsType>>(str);
+            return list;
         }
     }
 }
