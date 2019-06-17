88f9accca3a848447db402786cc66baca056f554
qq3490667734 <51558362+qq3490667734@users.noreply.github.com>
2019-06-17 14:00:25 +0800

    范涛涛

--- Brewmaster.Alcohol.Client/Brewmaster.Alcohol.Client/Controllers/搜索/GoodsSearch.cs
+++ Brewmaster.Alcohol.Client/Brewmaster.Alcohol.Client/Controllers/搜索/GoodsSearch.cs
@@ -14,42 +14,12 @@ namespace Brewmaster.Alcohol.Client.Controllers
     //商品搜索控制器
     public class GoodsSearch : Controller
     {
-        
         // GET: /<controller>/
         public IActionResult Index()
         {
-            ViewBag.data=GetBrand();
-            ViewBag.list=GetGoodsType();
-            ViewBag.goodsAll = GetGoodsAllListPage();
             return View();
         }
-
-        public List<Brand> GetBrand()
-        {
-            ApiHelper apiHelper=new ApiHelper();
-            var str= apiHelper.GetApiResult("get", "Brand/GetBrandName",null);
-            List<Brand> list = JsonConvert.DeserializeObject<List<Brand>>(str);
-            return list;
-        }
-
-        public List<GoodsType> GetGoodsType()
-        {
-            ApiHelper apiHelper = new ApiHelper();
-            var str = apiHelper.GetApiResult("get", "GoodsType/GetGoodsTypeName", null);
-             List<GoodsType> list = JsonConvert.DeserializeObject<List<GoodsType>>(str);
-            return list;
-        }
-
-        public List<GoodsAllListPage> GetGoodsAllListPage(string goodsName = "", string goodsDegree = "", int priceNow = 789, string brandName = "", string placeName = "", string aromaName = "", string typeName = "", int pageIndex = 1, int pageSize = 3)
-        {
-            ApiHelper apiHelper = new ApiHelper();
-            var str = apiHelper.GetApiResult("get",
-                "GoodsAll/GoodsAllListPage/?GoodsName=" + goodsName + "&goodsDegree=" + goodsDegree + "&priceNow" +
-                priceNow + "&BrandName=" + brandName + "&placeName=" + placeName + "&pageSize=" + pageSize +
-                "&pageIndex=" + pageIndex,null);
-            List<GoodsAllListPage> list = JsonConvert.DeserializeObject<List<GoodsAllListPage>>(str);
-            return list;
-        }
-
+        
+        
     }
 }
