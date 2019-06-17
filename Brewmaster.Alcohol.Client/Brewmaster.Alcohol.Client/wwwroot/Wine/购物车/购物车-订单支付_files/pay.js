531dca6b32b3c8f901f5463ed7d05d07a391f856
senyu0520 <51561807+senyu0520@users.noreply.github.com>
2019-06-17 09:11:53 +0800

    就好不接电话

---
+++ Brewmaster.Alcohol.Client/Brewmaster.Alcohol.Client/wwwroot/Wine/购物车/购物车-订单支付_files/pay.js
@@ -0,0 +1,9 @@
+﻿// JavaScript Document
+$(document).ready(function(e) {
+    $(".paymentList label").bind('click',function(){
+		var index = $(this).parent().index();
+		var top = 60*index;
+		$(this).parent().addClass("checked").siblings().removeClass("checked");
+		$("#payAmount").css('top',top+'px');
+	});
+});
