4efc926aeb0cc246a38a3a12ac0bf7f2e94c5de1
huakunkun <HKK20000115@162.com>
2019-06-17 09:33:09 +0800

    华坤坤

---
+++ Brewmaster.Alcohol.Api/Brewmaster.Alcohol.Model/ShopCart.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Brewmaster.Alcohol.Model
+{
+    /// <summary>
+    /// 购物车
+    /// </summary>
+    public class ShopCart
+    {
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 商品ID
+        /// </summary>
+        public int GoodsId
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// 用户Id
+        /// </summary>
+        public int UsersId
+        {
+            get; set;
+        }
+    }
+}
