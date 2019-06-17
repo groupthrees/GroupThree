daa2b91204da1309a488ef07c8a39f3b4472ada3
senyu0520 <51561807+senyu0520@users.noreply.github.com>
2019-06-17 11:32:31 +0800

    王森玉

---
+++ Brewmaster.Alcohol.Client/Brewmaster.Alcohol.Client/Controllers/购物车/ShopController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Brewmaster.Alcohol.Client.Controllers.购物车
+{
+    public class ShopController : Controller
+    {
+        /// <summary>
+        /// 购物车界面
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult ShopIndex()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 订单信息页面
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult OrderIndex()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 支付界面
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult PaymentIndex()
+        {
+            return View();
+        }
+
+    }
+}
