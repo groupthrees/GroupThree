6ee9a47e45c3bf7b7a001d04bf867f8d06a7f4f1
senyu0520 <51561807+senyu0520@users.noreply.github.com>
2019-06-17 15:32:14 +0800

    方法

---
+++ Brewmaster.Alcohol.Api/Brewmaster.Alcohol.IRepository/IAddressRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Brewmaster.Alcohol.Model;
+namespace Brewmaster.Alcohol.IRepository
+{
+    /// <summary>
+    /// 收货地址接口
+    /// </summary>
+    public interface IAddressRepository
+    {
+        /// <summary>
+        /// 添加收货地址
+        /// </summary>
+        /// <param name="address"></param>
+        /// <returns></returns>
+        int AddAddress(Address address);
+    }
+}
