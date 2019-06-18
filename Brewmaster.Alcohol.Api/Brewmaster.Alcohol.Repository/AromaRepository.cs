8cf7e62d9107e2cea7675edf141d794b214e7574
huakunkun <HKK20000115@162.com>
2019-06-19 07:07:28 +0800

    华坤坤

---
+++ Brewmaster.Alcohol.Api/Brewmaster.Alcohol.Repository/AromaRepository.cs
@@ -0,0 +1,31 @@
+﻿using Brewmaster.Alcohol.IRepository;
+using Brewmaster.Alcohol.Model;
+using Dapper;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Brewmaster.Alcohol.Repository
+{
+    public class AromaRepository: IAromaRepository
+    {
+        //数据库连接
+        private static string connStr = "Server=169.254.241.82;Database=alcohol;Uid=root;Pwd=1064519100;";
+
+        /// <summary>
+        /// 查询商品香型
+        /// </summary>
+        /// <returns></returns>
+        public List<Aroma> GetAroma()
+        {
+            using (MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                string sql = "select * from Aroma";
+                var result = conn.Query<Aroma>(sql).ToList();
+                return result;
+            }
+        }
+    }
+}
