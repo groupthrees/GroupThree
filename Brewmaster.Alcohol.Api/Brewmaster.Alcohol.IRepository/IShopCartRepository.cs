2cbddfa75f73f782c869ea2fb7dea0f3b710d03c
DESKTOP-A0CI09M <1064519100@qq.com>
2019-06-19 17:16:42 +0800

    11

--- Brewmaster.Alcohol.Api/Brewmaster.Alcohol.IRepository/IShopCartRepository.cs
+++ Brewmaster.Alcohol.Api/Brewmaster.Alcohol.IRepository/IShopCartRepository.cs
@@ -2,7 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-
+using Brewmaster.Alcohol.Model;
 namespace Brewmaster.Alcohol.IRepository
 {
     public interface IShopCartRepository
