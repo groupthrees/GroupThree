9281ffee74f7cb315ac9802cde3ec6f5ab3cc45a
senyu0520 <51561807+senyu0520@users.noreply.github.com>
2019-06-19 14:06:57 +0800

    王森玉

---
+++ Brewmaster.Alcohol.Api/Brewmaster.Alcohol.Model/Dto/订单Dto/orderImg.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Brewmaster.Alcohol.Model.Dto.订单Dto
+{
+    public class orderImg
+    {
+        /// <summary>
+        /// 订单Id
+        /// </summary>
+        public int ordersid { get; set; }
+        /// <summary>
+        /// 图片
+        /// </summary>
+        public string img { get; set; }
+    }
+}
