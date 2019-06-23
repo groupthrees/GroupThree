d0d1786b24606f9f35a24706cce2d03d0a3a712f
GitHubCorderLai <51562165+GitHubCorderLai@users.noreply.github.com>
2019-06-23 20:14:41 +0800

    1

--- Brewmaster.Alcohol.Api/Brewmaster.Alcohol.Repository/IndentRepository.cs
+++ Brewmaster.Alcohol.Api/Brewmaster.Alcohol.Repository/IndentRepository.cs
@@ -16,7 +16,7 @@ namespace Brewmaster.Alcohol.Repository
     {
 
         //数据库连接
-        private static string connStr = "Server=169.254.241.82;Database=alcohol;Uid=root;Pwd=1064519100;";
+        private static string connStr = "Server=169.254.200.110;Database=alcohol;Uid=root;Pwd=123456;";
 
         /// <summary>
         ///订单显示
--- Brewmaster.Alcohol.Api/Brewmaster.Alcohol.Repository/ShopCartRepository.cs
+++ Brewmaster.Alcohol.Api/Brewmaster.Alcohol.Repository/ShopCartRepository.cs
@@ -13,7 +13,7 @@ namespace Brewmaster.Alcohol.Repository
     public class ShopCartRepository: IShopCartRepository
     {
         //数据库连接
-        private static string connStr = "Server=169.254.241.82;Database=alcohol;Uid=root;Pwd=1064519100;";
+        private static string connStr = "Server=169.254.200.110;Database=alcohol;Uid=root;Pwd=123456;";
 
 
         /// <summary>
