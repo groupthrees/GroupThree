4c8b79da3cc2884f10a7a7b9facf83ded12c3ff6
GitHubCorderLai <51562165+GitHubCorderLai@users.noreply.github.com>
2019-06-28 18:55:11 +0800

    0

---
+++ Brewmaster.Alcohol.Back/Brewmaster.Alcohol.Back/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Brewmaster.Alcohol.Back.Models;
+using Brewmaster.Alcohol.Cache;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Brewmaster.Alcohol.Back.Controllers
+{
+    public class HomeController : Controller
+    {
+        [MyAuthorizeAttribute]
+        public IActionResult Index()
+        {
+            var tmpUser1 = RedisHelper.Get<UserInfo>("UserName");
+            if (tmpUser1 != null)
+            {
+                ViewBag.list = tmpUser1.PermissionList;
+                ViewData["Modules"]= tmpUser1.PermissionList;
+            }
+
+            return View();
+        }
+    }
+}
